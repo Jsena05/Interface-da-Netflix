60706888bbd5313914f0a8fa5b73346cf7ac9f98
Jsena05 <victor.sena15@hotmail.com>
2022-04-02 18:33:43 -0300

    commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 4
+        },
+        800: {
+            items: 5
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
